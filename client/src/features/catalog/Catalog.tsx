2634c799bd4e25f6ee69a5b66f1f4076dbc5ccb3
Shreyas Shetty <t_shreyas@vebuin.com>
2025-02-18 18:41:08 +0900

    End of section 4

--- client/src/features/catalog/Catalog.tsx
+++ client/src/features/catalog/Catalog.tsx
@@ -1,11 +1,16 @@
-import { Product } from "../../app/models/products";
+import { useEffect, useState } from "react";
 import ProductList from "./ProductList";
+import { Product } from "../../app/models/products";
 
-type Props = {
-  products: Product[];
-}
+export default function Catalog() {
+  const [products, setProducts] = useState<Product[]>([]);
+  
+  useEffect(() => {
+    fetch('https://localhost:5001/api/products')
+    .then(response => response.json())
+    .then(data => setProducts(data));
+  },[]);
 
-export default function Catalog({products}: Props) {
   return (
     <>
       <ProductList products={products}/>
